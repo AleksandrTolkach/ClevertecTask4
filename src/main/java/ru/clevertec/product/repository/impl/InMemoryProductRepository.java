a42292a3a4931c7d2d2704433520cb19641144aa
@a.toukach <shuuurick@mail.ru>
2023-11-02 00:14:51 +0300

    feature: add InMemoryProductRepositoryTest

--- src/main/java/ru/clevertec/product/repository/impl/InMemoryProductRepository.java
+++ src/main/java/ru/clevertec/product/repository/impl/InMemoryProductRepository.java
@@ -1,5 +1,7 @@
 package ru.clevertec.product.repository.impl;
 
+import lombok.AccessLevel;
+import lombok.NoArgsConstructor;
 import ru.clevertec.product.entity.Product;
 import ru.clevertec.product.repository.ProductRepository;
 
@@ -7,7 +9,10 @@ import java.util.List;
 import java.util.Optional;
 import java.util.UUID;
 
+@NoArgsConstructor(access = AccessLevel.PRIVATE)
 public class InMemoryProductRepository implements ProductRepository {
+
+    private static final InMemoryProductRepository instance = new InMemoryProductRepository();
 
     @Override
     public Optional<Product> findById(UUID uuid) {
@@ -28,4 +33,8 @@ public class InMemoryProductRepository implements ProductRepository {
     public void delete(UUID uuid) {
 
     }
+
+    public static ProductRepository getInstance() {
+        return instance;
+    };
 }
